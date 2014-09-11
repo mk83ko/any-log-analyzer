9dfd48502ed62c1389fa1642d9a65e8d9418d560
Markus Kokott <markus.kokott@gmail.com>
2014-09-11 21:50:22 +0200

    minor refactoring + adding ConsolePrinter as first output report class

---
+++ AnyLogAnalyzerCore/ReportGenerator/ConsolePrinter.cs
@@ -0,0 +1,35 @@
+﻿using Mkko.AnyLogAnalyzerData;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mkko.AnyLogAnalyzerCore
+{
+    public class ConsolePrinter : IReportGenerator
+    {
+        public void CreateReport(SortedSet<LogEvent> events)
+        {
+            foreach (LogEvent logEvent in events)
+            {
+                string output = logEvent.Category + " (" + logEvent.Timestamp.ToString() + ", line " + logEvent.Element.LineNumber + "):\r\n";
+                foreach (string key in logEvent.GetMetadataKeys())
+                {
+                    output += "\t*" + key + ": ";
+                    List<string> values = new List<string>();
+                    logEvent.getMetadata(key, out values);
+                    foreach (string value in values)
+                    {
+                        output += value + ",";
+                    }
+                    output = output.Substring(0, output.Length - 2) + "\r\n";
+                }
+
+                output += "\t(message: " + logEvent.Element.LogMessage + ")";
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
